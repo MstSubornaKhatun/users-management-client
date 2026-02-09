0b6be28d5650e2b67d20c29dff7e703f78b6d22e
suborna33 <152749142+suborna33@users.noreply.github.com>
2026-02-09 10:32:42 +0600

    user management

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -2,7 +2,7 @@ import React, { use } from 'react';
 
 const Users = ({usersPromise}) => {
     const users = use(usersPromise);
-    console.log(users); // (3) [{…}, {…}, {…}]
+    console.log(users); 
 
     const handleAddUser = e => {
         e.preventDefault();
@@ -10,6 +10,20 @@ const Users = ({usersPromise}) => {
         const email = e.target.email.value;
         const user = {name, email}
         console.log(user)
+
+
+        // create user in the server
+        fetch('http://localhost:3000/users',{
+            method: "POST",
+            headers: {
+                    "Content-Type": "application/json",
+                 },
+            body: JSON.stringify(user)
+        })
+        .then(res=>res.json())
+        .then(data => {
+            console.log('data after post', data)
+        })
     }
 
     return (
